ae9d16b28526c97060ebe60f45a784d2504e6ad5
Martin Heir <mbheir@gmail.com>
2020-02-17 15:54:42 +0100

    fixed all bugs and changed a few functions

--- source/queue.c
+++ source/queue.c
@@ -24,41 +24,25 @@ void queueAddOrderToQueue(int floor, HardwareOrder order, Elevator *elevator) {
     case HARDWARE_ORDER_INSIDE:
         elevator->queue.inside[floor] = true;
         break;
-    }
-}
-
-bool queueOrderInCurrentDirection(int order_floor, int current_floor, bool last_direction){
-	bool potential_direction;
-	if (order_floor>current_floor){
-		potential_direction = true;
 	}
-	else if(order_floor<current_floor){
-		potential_direction = false;
-	}
-	else return true;
-		
-	return (last_direction && potential_direction);  
-
 }
 
 bool queueOrderExist(Elevator *elevator){
-	for(unsigned int i=0, i<4,i++){
+	for(unsigned int i=0; i<HARDWARE_NUMBER_OF_FLOORS;i++){
 		if(elevator->queue.up[i] || elevator->queue.down[i] || elevator->queue.inside[i]){
 			return true;
 		}
 	}
 	return false;
-
 }
 
 void queueClearAllOrdersOnFloor(int floor, Elevator *elevator){
-	for(unsigned int i =0;i<4,i++){
-		elevator->queue.up[i]=false;
-		elevator->queue.down[i]=false;
-		elevator->queue.inside[i]=false;
-	}
+	elevator->queue.up[floor]=false;
+	elevator->queue.down[floor]=false;
+	elevator->queue.inside[floor]=false;
 }
 
-void queueRemoveElementFromQueue(array *queue, int floor){
-	*queue[floor]=false;
+
+void queueRemoveElementFromQueue(bool queue[], int floor){
+	queue[floor]=false;
 }
