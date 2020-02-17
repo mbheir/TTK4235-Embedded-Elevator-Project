4330e1c3aab6ebdb889747798d35916098020140
Håvard <hava.brenne@gmail.com>
2020-02-17 15:09:50 +0100

    Removed unneccesary functions from Light Control

--- source/LightControl.h
+++ source/LightControl.h
@@ -1,3 +1,3 @@
 #include "stdbool.h"
 #include "hardware.h"
-void lightUpdateFromQueue(int *up_queue, int *down_queue, int *inside_queue)
+void lightUpdateFromQueue(int *up_queue, int *down_queue, int *inside_queue);
