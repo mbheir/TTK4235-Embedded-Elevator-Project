0f8ac4c18adcb0e79862068c7417c1788594148f
Martin Heir <mbheir@gmail.com>
2020-02-17 15:04:14 +0100

    moved contents of elevator.h into queue.h to prevent build error

--- source/elevator.h
+++ source/elevator.h
@@ -8,32 +8,31 @@
 #include <stdbool.h>
 #include "queue.h"
 
-
-typedef enum State { 
-    STANDBY, 
-    DOORS_OPEN, 
-    GOING_UP, 
-    GOING_DOWN, 
-    EMERGENCY
-} State;
-
-
-typedef struct Queue {
-    bool up[4];
-    bool down[4];
-    bool inside[4];
-} Queue;
+// typedef enum State { 
+//     STANDBY, 
+//     DOORS_OPEN, 
+//     GOING_UP, 
+//     GOING_DOWN, 
+//     EMERGENCY
+// } State;
 
 
+// typedef struct Queue {
+//     bool up[4];
+//     bool down[4];
+//     bool inside[4];
+// } Queue;
 
-typedef struct Elevator {
-    State state;    
-    int current_floor;
-    bool floor_sensor_triggered;
-    bool emergency_stop;
-    bool door_open;
-    bool last_direction;
-    Queue queue;
-}  Elevator;
+
+
+// typedef struct Elevator {
+//     State state;    
+//     int current_floor;
+//     bool floor_sensor_triggered;
+//     bool emergency_stop;
+//     bool door_open;
+//     bool last_direction;
+//     Queue queue;
+// }  Elevator;
 
 #endif
--- source/queue.h
+++ source/queue.h
@@ -6,7 +6,35 @@
 
 #include <stdbool.h>
 #include "hardware.h"
-#include "elevator.h"
+//#include "elevator.h"
+
+
+typedef enum State { 
+    STANDBY, 
+    DOORS_OPEN, 
+    GOING_UP, 
+    GOING_DOWN, 
+    EMERGENCY
+} State;
+
+
+typedef struct Queue {
+    bool up[4];
+    bool down[4];
+    bool inside[4];
+} Queue;
+
+
+
+typedef struct Elevator {
+    State state;    
+    int current_floor;
+    bool floor_sensor_triggered;
+    bool emergency_stop;
+    bool door_open;
+    bool last_direction;
+    Queue queue;
+}  Elevator;
 
 
 void queueUpdateFromButtons(Elevator *elevator);
