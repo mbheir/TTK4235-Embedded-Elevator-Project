a4cf57aab2ca4e966e2d58ffac5609dcbcc66a3c
Håvard <hava.brenne@gmail.com>
2020-02-18 10:44:13 +0100

    Revert "fixed indicator light"
    This reverts commit 3a15777bf29ea6d1cd4055301acd81d50944e338.

--- source/LightControl.c
+++ source/LightControl.c
@@ -21,6 +21,7 @@ void lightUpdateFromQueue(bool up_queue[], bool down_queue[], bool inside_queue[
 			hardwareCommandOrderLight(i, HARDWARE_ORDER_INSIDE, 0);
 		}
 	}
+	
 }
 
 
@@ -28,7 +29,6 @@ bool checkAndUpdateFloor(Elevator *elevator) {
 	for (int floor = 0; floor < HARDWARE_NUMBER_OF_FLOORS; floor++) {
 		if (hardwareReadFloorSensor(floor)) {
 			elevator->current_floor = floor;
-			hardwareCommandFloorIndicatorOn(elevator->current_floor);
 			return true;
 		}
 	}
