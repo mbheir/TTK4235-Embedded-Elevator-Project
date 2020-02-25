15fc973b272d6612262780694edb2df0a50740dd
Håvard <hava.brenne@gmail.com>
2020-02-25 11:42:19 +0100

    Added doxygen description to Lightcontrol. Fixed some typos

--- source/LightControl.h
+++ source/LightControl.h
@@ -1,3 +1,7 @@
+/**
+ * @file
+ * @brief Function for updating all lights based on orders in queue
+ */
 
 #ifndef LIGHTCONTROL_H
 #define LIGHTCONTROL_H
@@ -5,6 +9,10 @@
 #include "hardware.h"
 #include "queue.h"
 
+/**
+ * @brief Turns on or off light based on orders in queue
+ * @param[in] queue Queue with orders
+ */
 void lightUpdateFromQueue(Queue queue);
 
 #endif
